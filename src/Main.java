85d61027bc363474d22f687490537c784829606b
local.brawit <phoksosatsyabrawit@gmail.com>
2026-03-01 00:38:01 +0700

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.sql.PooledConnection;
 import java.lang.String;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args){
         String myName = "local.brawit";
         int myNum = 26;
+        Point p1 = new Point(1,1);
+        Point p3 = p1;
+        p1.x = 3;
         System.out.println("Name: " + myName);
         System.out.println("Age: " + myNum);
+        System.out.println(p1);
     }
 }
