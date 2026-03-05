90e0866a902d3f07e8bbf569c86c2f3aa75ff445
local.brawit <phoksosatsyabrawit@gmail.com>
2026-03-05 22:28:43 +0700

    Updated

--- src/Main.java
+++ src/Main.java
@@ -7,12 +7,20 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args){
         String myName = "local.brawit";
+        String myPath = "C:\\Users\\local.brawit\\Outlook\\archives.pst";
         int myNum = 26;
+        double result = Math.floor(1.1f);
+        int myRes = (int) Math.floor(2.1f);
+        double myResu = Math.random() * 2;
         Point p1 = new Point(1,1);
         Point p2 = p1;
         p1.x = 3;
         System.out.println("Name: " + myName);
         System.out.println("Age: " + myNum);
         System.out.println(p1);
+        System.out.println(myPath);
+        System.out.println(result);
+        System.out.println(myRes);
+        System.out.println(myResu);
     }
 }
